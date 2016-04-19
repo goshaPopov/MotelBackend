27951216407dc8424fbfa13d1e5dccad5e37879b
Georgiy <gorge39@gmail.com>
2016-04-19 20:42:04 +0500

    Initial commit

---
+++ src/main/java/com/popovgosha/motelbackend/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.popovgosha.motelbackend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Georgiy Popov on 19.04.2016.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.popovgosha.motelbackend")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        converters.add(converter);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/resources/static/**").addResourceLocations("/resources/static/");
+    }
+
+}
